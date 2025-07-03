d04a04c6f096e2b3b9e315085dae82e283374712
ckaanf <skywlstn777@gmail.com>
2025-07-03 13:41:25 +0900

    fix: review resolve conversation

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,16 +24,16 @@ subprojects {
         withJavadocJar()
     }
 
-    tasks.test {
-        useJUnitPlatform()
-    }
-
     dependencies {
         testImplementation(platform("org.junit:junit-bom:5.9.2"))
         testImplementation("org.junit.jupiter:junit-jupiter")
         testImplementation("org.mockito:mockito-core:5.7.0")
         testImplementation("org.mockito:mockito-junit-jupiter:5.1.1")
         testImplementation("org.assertj:assertj-core:3.24.2")
+    }
+
+    tasks.test {
+        useJUnitPlatform()
     }
 
     publishing {
